877a8b815ef276a64fd515b8ba13ff179bc83d2a
Corneliu Iancu <corneliu.iancu27@gmail.com>
2015-07-13 22:33:18 +0300

    lu1307;

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,30 +1,20 @@
 /* 
-    Created on : May 26, 2015, 11:42:49 PM
-    Author     : Cornelius
-*/
+ Created on : May 26, 2015, 11:42:49 PM
+ Author     : Cornelius
+ */
 
-var application = application || {} ;
+var application = application || {};
 
 application.global = {
-  
-    enableMenu : function(){
-        
-        $('.root-categories > li > a').mouseenter(function(){
-            $('.root-categories > li').removeClass('open');
-            $(this).parent().addClass('open');
-        });
-          
-        $('.open-cat').mouseleave(function(){
-            $(this).parent().removeClass('open');
-        });
+    enableMenu: function() {
+
     },
-  
-    initialize : function(){
+    initialize: function() {
         this.enableMenu();
-    console.info('Application was initialized');
-}
+        console.info('Application was initialized');
+    }
 };
 
-$().ready(function(){
+$().ready(function() {
     application.global.initialize();
 });
