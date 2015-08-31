0f6e2e75053a2e09512b90a6c68d82cdccd19325
Socratemus <corneliu.iancu27@gmail.com>
2015-08-31 06:56:17 +0000

    updat

--- public/javascript/_.js
+++ public/javascript/_.js
@@ -3,22 +3,9 @@ var _ = {
         console.debug('this is a test');
     },
     
-    menu : function(){
-        $(document).on('click' , '#MainHandler' , function(e){
-            $('content').toggleClass('col-md-offset-4');
-            $('content').toggleClass('pushed');
-           
-            e.preventDefault();return false;
-        });
-        $(document).on('click' , 'content.pushed' , function(e){
-            $('content').toggleClass('col-md-offset-4');
-            $('content').toggleClass('pushed');
-            e.preventDefault();return false;
-        })
-    },
     
     init : function(){
         this.test();
-        this.menu();
+       
     }
 };
